f8e5775632cae7728037dd3eef95ddf5a6907bfa
divya-mohan0209 <divya.mohan0209@gmail.com>
2020-11-23 13:49:38 +0530

    Create build-sphinx-docs.sh

---
+++ build-sphinx-docs.sh
@@ -0,0 +1,26 @@
+set -e
+trap "{ echo -e '\033[31mFAILED\033[0m'; }" ERR
+
+[[ $1 == 'fast' ]] && CPUS='-j auto' || CPUS=''
+
+docs_conf_dir="main/source"
+out_dir="master/website/static/rucio"
+
+# Cleanup pre-existing temporary generated files.
+rm -rf "master/website/static/rucio"
+
+# Cleanup previous output.
+rm -rf "${out_dir}"
+
+# Navigate to the checked out rucio repository & backup
+cd rucio/doc
+mv source source_copy
+cp -R "$docs_conf_dir" .
+
+# Regenerate docs.
+sphinx-build -B html . "${out_dir}" -W --keep-going $CPUS
+
+echo
+echo Index Page:
+echo "file://${out_dir}/html/index.html"
+echo
