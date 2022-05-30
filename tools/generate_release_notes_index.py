b072d20bc9a696ed5ed549824fd27c16618fac9c
Joel Dierkes <joel.dierkes@ovgu.de>
2022-05-30 15:03:18 +0200

    Add the minor release names to the release notes list #119
    The release notes are grouped by minor releases. These releases have
    names, which should be displayed in the title of the minor releses.

--- tools/generate_release_notes_index.py
+++ tools/generate_release_notes_index.py
@@ -4,6 +4,7 @@ import pathlib
 import re
 from collections import defaultdict
 from typing import Dict, List
+import yaml
 
 import jinja2
 
@@ -26,6 +27,39 @@ def map_post_version_sort_to_number(stem: str) -> int:
 
 
 def render_templates(templates_dir: str, output_path: pathlib.Path):
+    def minor_release_get_title(path: str, version: str) -> str:
+        """
+        Returns the title for a minor release. If this title does not exist it
+        returns the version string itself.
+
+        :param path: The path to the folder containing the release informations.
+        :param version: A major and minor version of a release. (e.g. "1.27")
+        :returns: The coresponsing release title. This is just the version string in
+            case no title exist.
+        """
+
+        # The minor release note title should be in the first release of the
+        # minor history. Due to inconsistencies this is not always the case, so
+        # we have to hardcode the missing ones.
+        HARD_CODED_RELEASE_NOTE_TITLE = {
+            "1.19": "1.19 Fantastic Donkeys",
+            "1.13": "1.13 Donkerine"
+        }
+        if version in HARD_CODED_RELEASE_NOTE_TITLE:
+            return HARD_CODED_RELEASE_NOTE_TITLE[version]
+
+        release_title_file_path = os.path.join(output_path / path, version + ".0.md")
+
+        if not os.path.exists(release_title_file_path):
+            return version
+
+        with open(release_title_file_path, 'r') as f:
+            content = f.read().split("---")[1]
+        data = yaml.safe_load(content)
+
+        minor_version_title = data["title"].replace("\"", "").replace("'", "").split(".0 ")[1]
+        return version + " " + minor_version_title
+
     def index_item(path: pathlib.Path):
         return {"stem": path.stem, "path": str(path.relative_to(output_path))}
 
@@ -57,6 +91,7 @@ def render_templates(templates_dir: str, output_path: pathlib.Path):
         loader=jinja2.FileSystemLoader(templates_dir)
     )
     jinja_env.globals["index"] = index_func
+    jinja_env.globals["minor_release_get_title"] = minor_release_get_title
     for tpl in pathlib.Path(templates_dir).iterdir():
         # render all templates with .md as the first suffix and only non-hidden files
         if tpl.suffixes[0] == ".md" and not tpl.name.startswith("."):
