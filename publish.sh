e7593fa481b330cd1bc79db99de4ee69dc6f270f
Joel Dierkes <joel.dierkes@cern.ch>
2022-06-15 10:01:41 +0200

    Deploy the html and yaml assets
    The html and yaml assets are auto generated. Since we don't want to
    track them in the VS, we ignored them. This however is a problem, since
    the deployment is done via a commit on a specific branch. The
    `.gitignore` file prevented the upload of the assets.

--- publish.sh
+++ publish.sh
@@ -22,6 +22,13 @@ cp -r "${INPUT_DIRECTORY}" "${OUTPUT_DIRECTORY}"
 mv output.git "${OUTPUT_DIRECTORY}/.git"
 cd "${OUTPUT_DIRECTORY}"
 touch .nojekyll
+
+# We want to add the assets to the deployment, however we don't want to have the
+# changes in the versioning system. Deleting the .gitignore files will add the
+# assets to the deployment.
+rm html/.gitignore
+rm yaml/.gitignore
+
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git add .
