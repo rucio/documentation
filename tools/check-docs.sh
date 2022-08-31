db4d0a03c2ddcbac4a40283b886dd54a65f3cf48
Joel Dierkes <joel.dierkes@cern.ch>
2022-08-31 12:49:46 +0200

    Disable the md checks for auto generated files

--- tools/check-docs.sh
+++ tools/check-docs.sh
@@ -4,11 +4,12 @@
 # shellcheck disable=SC2046
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+FIND_IGNORE_CONFIG='-not -path "*/website/*" -not -path "*/docs/release-notes/*" -not -name "component_leads.md" -not -name "releasepolicy.md" -not -path "*/docs/bin/*" -not -path "*/docs/release-notes.md"'
 
-mdl --config "$SCRIPT_DIR"/.mdl/mdlrc $(find "$SCRIPT_DIR"/.. -name "*.md" -not -path "*/website/*" -not -path "*/docs/release-notes/*" -not -name "component_leads.md" -not -name "releasepolicy.md")
+mdl --config "$SCRIPT_DIR"/.mdl/mdlrc $(find "$SCRIPT_DIR"/.. -name "*.md $FIND_IGNORE_CONFIG")
 exit_code=$?
 
-mdl --config "$SCRIPT_DIR"/.mdl/mdlrc_long_lines $(find "$SCRIPT_DIR"/.. -name "*.md" -and \( -path "*/docs/release-notes/*" -or -name "releasepolicy.md" \)) && [ "$exit_code" -eq "0" ]
+mdl --config "$SCRIPT_DIR"/.mdl/mdlrc_long_lines $(find "$SCRIPT_DIR"/.. -name "*.md" -and -name "releasepolicy.md" ) && [ "$exit_code" -eq "0" ]
 exit_code=$?
 
 mdl --config "$SCRIPT_DIR"/.mdl/mdlrc --rules ~MD013 "$SCRIPT_DIR"/../docs/component_leads.md && [ "$exit_code" -eq "0" ]
