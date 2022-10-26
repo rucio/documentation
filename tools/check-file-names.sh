f7b1df06a193f149864e4477f199509023e3c45c
Rizart Dona <rizart.dona@gmail.com>
2022-10-26 14:21:38 +0200

    allow numerical values in .md filenames

--- tools/check-file-names.sh
+++ tools/check-file-names.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 exit_code=0
 
-for f in $(find "$SCRIPT_DIR"/../docs -name "*.md" -not -path "*/release-notes/*" -not -path "*/bin/*" -not -path "*/release-notes.md" -printf "%f\n" | grep --invert-match '^[a-z_]*\.md$'); do
+for f in $(find "$SCRIPT_DIR"/../docs -name "*.md" -not -path "*/release-notes/*" -not -path "*/bin/*" -not -path "*/release-notes.md" -printf "%f\n" | grep --invert-match '^[a-z0-9_]*\.md$'); do
     echo "$f does not comply with the snakecase convention!"
     exit_code=1
 done
