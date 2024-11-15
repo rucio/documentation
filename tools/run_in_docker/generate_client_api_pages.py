713cd5c87eaa1c7374b179ecf85bd671cb7071a2
voetberg <magpie127@gmail.com>
2024-11-15 08:53:06 -0600

    Correct formatting issues

--- tools/run_in_docker/generate_client_api_pages.py
+++ tools/run_in_docker/generate_client_api_pages.py
@@ -1,21 +1,22 @@
 """Generate the code reference pages and navigation."""
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 import mkdocs_gen_files
 
 src = Path("rucio/lib/rucio/client/")
 root = src.parent
 doc_path = "/auto_generated/client_api"
 files = os.listdir(doc_path)
-for file in files: 
+for file in files:
     os.remove(f"{doc_path}/{file}")
 
 
 py_files = [f for f in list(src.rglob("*.py")) if "__init__.py" not in f.name]
 
 for path in py_files:
-    module_name = path.name.split('.py')[0]
+    module_name = path.name.split(".py")[0]
     full_doc_path = Path(f"{doc_path}/{module_name}.md")
 
     with mkdocs_gen_files.open(full_doc_path, "a") as fd:
