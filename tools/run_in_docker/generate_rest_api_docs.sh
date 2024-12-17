152b87b3396b9b6f01ab43ffacc1656e4903a964
voetberg <magpie127@gmail.com>
2024-12-17 08:53:21 -0600

    Copy documentation files from main repo into doc repo

--- tools/run_in_docker/generate_rest_api_docs.sh
+++ tools/run_in_docker/generate_rest_api_docs.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo "Generating the Rest Api Docs..."
-rucio/tools/generate_rest_api_doc.py > /auto_generated/rest_api_doc_spec.yaml
+/usr/bin/env python3 generate_rest_api_docs.py > /auto_generated/rest_api_doc_spec.yaml
