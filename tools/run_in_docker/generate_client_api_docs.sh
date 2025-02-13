191f2dd1ca07a2658de0be306ac81b740b1920e0
voetberg <magpie127@gmail.com>
2025-02-13 08:29:08 -0600

    Fix link to client docs - correct name

--- tools/run_in_docker/generate_client_api_docs.sh
+++ tools/run_in_docker/generate_client_api_docs.sh
@@ -11,6 +11,6 @@ pip install --upgrade mkdocs mkdocs-gen-files mkdocstrings-python mkdocs-materia
 mkdir -p /auto_generated/client_api
 
 python3 generate_client_api_pages.py
-mkdocs build --clean --no-directory-urls --config-file mkdocs.yml --site-dir api_site
+mkdocs build --clean --no-directory-urls --config-file mkdocs.yml --site-dir client_api
 
-cp -r api_site /auto_generated/
+cp -r client_api /auto_generated/
