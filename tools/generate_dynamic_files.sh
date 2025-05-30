c053302866532afc90f2d6df9d4c2ca8b7decd18
Dimitris Xenakis <d.xenakis@ieee.org>
2025-05-30 20:57:02 +0200

    Refactor build scripts to handle local Rucio source or clone

--- tools/generate_dynamic_files.sh
+++ tools/generate_dynamic_files.sh
@@ -5,5 +5,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 docker run --rm \
        --volume "$SCRIPT_DIR/run_in_docker:/run_in_docker:rw" \
        --volume "$SCRIPT_DIR/auto_generated:/auto_generated:rw" \
+       ${MOUNT_RUCIO_PATH:+--volume "$MOUNT_RUCIO_PATH:/run_in_docker/rucio:ro"} \
        ghcr.io/rucio/rucio/rucio-autotest:alma9-python3.10 \
        /bin/bash -c /run_in_docker/generate_docs.sh
