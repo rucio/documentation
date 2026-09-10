066f3443c66085f887cd7e95473d3b7e418fdec5
GitHub Action <action@github.com>
2026-09-10 08:43:24 +0000

    Documentation autoupdate

---
+++ assets/js/61567.1786a00d.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([["61567"],{85698(e,t,r){r.d(t,{diagram:()=>l});var s=r(18312);r(64918),r(96755),r(35869),r(841),r(72391),r(43247),r(82735),r(5616),r(16163),r(97827),r(43002),r(60739),r(36320),r(62217),r(92941),r(86348),r(81177),r(31293);var a=(0,r(86827).K)(e=>`${(0,s.tM)(e)}
+  .swimlane.cluster:not([data-color-id]) rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),l=(0,s.ur)({styles:a})}}]);
